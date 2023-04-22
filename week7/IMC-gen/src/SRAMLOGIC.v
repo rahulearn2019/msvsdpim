3409f77389d0f1e24871ea1ca39876c659a65d87
Rahul Tiwari <50217106+rahulearn2019@users.noreply.github.com>
2023-04-22 16:09:58 +0530

    Add files via upload

---
+++ week7/IMC-gen/src/SRAMLOGIC.v
@@ -0,0 +1,15 @@
+module SRAMLOGIC(
+		input WWL,
+		input RWL,
+		input WBL1,
+		input WBLB1,
+		input WBL2,
+		input WBLB2,
+		input RBLprecharge,
+		input RBLprechargeEnable,
+		output Q1, 
+		output Q2,
+		output NOR,
+		output OR
+	);
+endmodule
